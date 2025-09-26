54e25094c72c71bc97d3adae97edb839f726a916
Djamil <dsmoonweb@gmail.com>
2025-09-26 21:54:28 +0200

    Implement reset password and create HomeController

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -45,12 +45,11 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('email', EmailType::class)
-            // ->add('roles')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les termes.',
                     ]),
                 ],
             ])
@@ -61,13 +60,13 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'max' => 30,
                     ]),
                 ],
             ])
@@ -79,7 +78,6 @@ class RegistrationFormType extends AbstractType
         $resolver->setDefaults([
             'data_class' => User::class,
             'adminChoice' => false,
-
         ]);
     }
 }
